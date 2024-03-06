cf316a5217a32d2d4048627d6515cc6c95e8efc3
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-03-07 00:43:16 +0530

    Fix import typo and update server response

--- frontend/src/slices/orderApiSlice.js
+++ frontend/src/slices/orderApiSlice.js
@@ -11,21 +11,19 @@ export const orderApiSlice = apiSlice.injectEndpoints({
       }),
     }),
     getOrderDetails: builder.query({
-      query: (oderId) => ({
-        url: `${ORDERS_URL}/${oderId}`,
+      query: (id) => ({
+        url: `${ORDERS_URL}/${id}`,
       }),
       keepUnusedDataFor: 5,
     }),
     payOrder: builder.mutation({
-      query: (orderId, details) => ({
+      query: ({ orderId, details }) => ({
         url: `${ORDERS_URL}/${orderId}/pay`,
         method: "PUT",
-        body: {
-          ...details,
-        },
+        body: details,
       }),
     }),
-    getPayPalClientId: builder.query({
+    getPaypalClientId: builder.query({
       query: () => ({
         url: PAYPAL_URL,
       }),
@@ -38,5 +36,5 @@ export const {
   useCreateOrderMutation,
   useGetOrderDetailsQuery,
   usePayOrderMutation,
-  useGetPayPalClientIdQuery,
+  useGetPaypalClientIdQuery,
 } = orderApiSlice;
