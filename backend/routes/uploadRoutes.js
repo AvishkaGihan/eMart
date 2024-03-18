2b3d832b40635fc922eecbda5cac5b923ce9eae0
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-03-18 23:36:52 +0530

    Upload product image

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,12 +1,11 @@
 import path from "path";
 import express from "express";
 import multer from "multer";
-
 const router = express.Router();
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cd(null, "uploads/");
+    cb(null, "uploads/");
   },
   filename(req, file, cb) {
     cb(
@@ -24,17 +23,17 @@ function checkFileType(file, cb) {
   if (extname && mimetype) {
     return cb(null, true);
   } else {
-    cb("Images only!");
+    cb({ message: "Images only!" });
   }
 }
 
-const uploads = multer({
+const upload = multer({
   storage,
 });
 
-router.post("/upload", uploads.single("image"), (req, res) => {
+router.post("/", upload.single("image"), (req, res) => {
   res.send({
-    message: "Image Uploaded",
+    message: "Image uploaded successfully",
     image: `/${req.file.path}`,
   });
 });
--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 dotenv.config();
 import connectDB from "./config/db.js";
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = process.env.PORT || 5000;
 
@@ -30,11 +30,19 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);
