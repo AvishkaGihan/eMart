45f27286fc3b8dec2d7f898dc562cc0329f96f2e
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-03-22 11:04:54 +0530

    Product carousel

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,16 @@
 import { Row, Col } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useGetProductsQuery } from "../slices/productApiSlice";
+import { Link } from "react-router-dom";
 import Product from "../components/Product";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 import Paginate from "../components/Paginate";
-import { useGetProductsQuery } from "../slices/productApiSlice";
+import ProductCarousel from "../components/ProductCarousel";
 
 const HomeScreen = () => {
   const { pageNumber, keyword } = useParams();
+
   const { data, isLoading, error } = useGetProductsQuery({
     keyword,
     pageNumber,
@@ -15,18 +18,22 @@ const HomeScreen = () => {
 
   return (
     <>
-      {keyword && (
-        <Link to="/" className="btn btn-light mb-4">
+      {!keyword ? (
+        <ProductCarousel />
+      ) : (
+        <Link to="/" className="btn btn-light">
           Go Back
         </Link>
       )}
       {isLoading ? (
         <Loader />
       ) : error ? (
-        <Message variant="danger">{error?.data?.message || error.data}</Message>
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
       ) : (
         <>
-          <h1>Latest Product</h1>
+          <h1>Latest Products</h1>
           <Row>
             {data.products.map((product) => (
               <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
