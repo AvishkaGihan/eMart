71f9829c4845a906b1abb623f252b6c97721e9ed
Avishka Gihan <87961869+AvishkaGihan@users.noreply.github.com>
2024-03-29 12:00:12 +0530

    Update styles and components

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -4,7 +4,7 @@ import { LinkContainer } from "react-router-bootstrap";
 const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
   return (
     pages > 1 && (
-      <Pagination>
+      <Pagination className="d-flex justify-content-center">
         {[...Array(pages).keys()].map((x) => (
           <LinkContainer
             key={x + 1}
@@ -16,7 +16,12 @@ const Paginate = ({ pages, page, isAdmin = false, keyword = "" }) => {
                 : `/admin/productlist/${x + 1}`
             }
           >
-            <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
+            <Pagination.Item
+              active={x + 1 === page}
+              className="custom-pagination-item"
+            >
+              {x + 1}
+            </Pagination.Item>
           </LinkContainer>
         ))}
       </Pagination>
--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -12,11 +12,18 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant="danger">{error?.data?.message || error.error}</Message>
   ) : (
-    <Carousel pause="hover" className="bg-primary mb-4">
+    <Carousel variant="dark" pause="hover" className="mb-4">
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
+            <div className="d-flex justify-content-center">
+              <Image
+                src={product.image}
+                alt={product.name}
+                fluid
+                className="center-image"
+              />
+            </div>
             <Carousel.Caption className="carousel-caption">
               <h2 className="text-white text-right">
                 {product.name} (${product.price})
