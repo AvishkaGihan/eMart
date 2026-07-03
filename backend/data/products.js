5b099e68e5f46c38106e175018d9da942a14f7ca
AvishkaGihan <avishkag18@gmail.com>
2026-07-03 10:29:01 +0530

    feat: add initial product seed data and global CSS styles for UI components

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,8 @@
 const products = [
   {
     name: "Airpods Wireless Bluetooth Headphones",
-    image: "/images/airpods.jpg",
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MV7N2?wid=572&hei=572&fmt=jpeg&qlt=95",
     description:
       "Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working",
     brand: "Apple",
@@ -13,7 +14,8 @@ const products = [
   },
   {
     name: "iPhone 11 Pro 256GB Memory",
-    image: "/images/phone.jpg",
+    image:
+      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-11-pro-midnight-green-select-2019?wid=470&hei=556&fmt=jpeg&qlt=95",
     description:
       "Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
     brand: "Apple",
@@ -25,7 +27,8 @@ const products = [
   },
   {
     name: "Cannon EOS 80D DSLR Camera",
-    image: "/images/camera.jpg",
+    image:
+      "https://m.media-amazon.com/images/I/71rXSVqET9L._AC_SL1500_.jpg",
     description:
       "Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design",
     brand: "Cannon",
@@ -37,7 +40,8 @@ const products = [
   },
   {
     name: "Sony Playstation 4 Pro White Version",
-    image: "/images/playstation.jpg",
+    image:
+      "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UF894,1000_QL80_.jpg",
     description:
       "The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music",
     brand: "Sony",
@@ -49,7 +53,8 @@ const products = [
   },
   {
     name: "Logitech G-Series Gaming Mouse",
-    image: "/images/mouse.jpg",
+    image:
+      "https://m.media-amazon.com/images/I/61mpMH5TzkL._AC_SL1500_.jpg",
     description:
       "Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse. The six programmable buttons allow customization for a smooth playing experience",
     brand: "Logitech",
@@ -61,7 +66,8 @@ const products = [
   },
   {
     name: "Amazon Echo Dot 3rd Generation",
-    image: "/images/alexa.jpg",
+    image:
+      "https://m.media-amazon.com/images/I/6182S7MYC2L._AC_SL1000_.jpg",
     description:
       "Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space",
     brand: "Amazon",
@@ -74,3 +80,4 @@ const products = [
 ];
 
 export default products;
+
